3d2934e96deb9863c15a9de51908134219aa2d9a
benjhardy <benjamin.m.hardy@vanderbilt.edu>
2022-08-10 10:57:21 -0500

    added embedded plot feature

--- uSolenoidgui.py
+++ uSolenoidgui.py
@@ -1,10 +1,47 @@
-import tkinter as tk
-import uSolenoid as us
 # Benjamin Hardy, 8-09-2022
 # Solenoid designer GUI
 # Visualize Solenoid design parameters
 #
 # most of this is from: https://realpython.com/python-gui-tkinter/
+# embedded plots: https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/#:~:text=Embedding%20the%20Plot%3A,a%20toolbar%20at%20the%20bottom.
+# tk.pack() options : https://www.tutorialspoint.com/python/tk_pack.htm
+# Imports:
+import tkinter as tk
+from matplotlib.figure import Figure
+import numpy as np
+import uSolenoid as us
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+
+
+def plot(N,dcoil,lcoil,dwire,freqs,alpha,beta,rho,sigma):
+    # the figure that will contain the plot
+    fig = Figure(figsize = (5, 5),
+                 dpi = 100)
+    sig = []
+    # update the class so that it handles vectors!
+    for f in freqs:
+        s = us.Coil(int(N.get()), float(dcoil.get()), float(lcoil.get()), float(dwire.get()), f, float(alpha.get()), float(beta.get()), float(rho.get()), float(sigma.get()))
+        sig.append(s.getSig())
+    
+    # adding the subplot
+    plot1 = fig.add_subplot(111)
+    # plotting the graph
+    plot1.plot(freqs,sig)
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,master = window)  
+    canvas.draw()
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+  
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,window)
+    toolbar.update()
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+
 window = tk.Tk()
 window.title('Solenoid Designer')
 greeting = tk.Label(text="Solenoid Designer")
@@ -48,9 +85,11 @@ sigma = tk.Entry()
 l.pack()
 sigma.insert(0,1)
 sigma.pack()
-# now create a button that takes all the values and spits out a plot inside the window
-
+# test array
+freqs = np.arange(300,950,100)
 button = tk.Button(
+    master = window,
+    command = lambda: plot(N,dcoil,lcoil,dwire,freqs,alpha,beta,rho,sigma),
     text="Graph Stuff",
     width=10,
     height=2,
